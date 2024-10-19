d26fc5ad2005f65bd15f79ae486cad185b162029
Pranavsolanki01 <pranavsolanki200@gmail.com>
2024-10-19 16:54:44 +0530

    Adding the services and the Migration

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,6 +1,25 @@
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interface;
+using CleanArchitecture.Infrastructure.Data;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// now resolve the dependency for the database connectivity or access
+builder.Services.AddDbContext<BlogDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+	throw new InvalidOperationException("Connection String 'DefaultConnection' not found") ));
+
+
+
+// registering the service for the dependency for the repositories
+// the dependency for the repositories is resolved by the AddScoped method or the AddTransient method
+// as i want to make the services are created each time they are requested, i will use the AddTransient method
+builder.Services.AddTransient<IBlogRepository, BlogRepository>();
+builder.Services.AddTransient<IBlogService, BlogService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- CleanArchitecture.Infrastructure/Repositories/BlogRepository.cs
+++ CleanArchitecture.Infrastructure/Repositories/BlogRepository.cs
@@ -12,32 +12,32 @@ namespace CleanArchitecture.Infrastructure.Repositories
 {
 	public class BlogRepository : IBlogRepository
 	{
-		private readonly BlogDbContext _blogDbContext;
+		private readonly BlogDbContext _DbContext;
 		public BlogRepository(BlogDbContext blogDbContext)
 		{
-			_blogDbContext = blogDbContext;
+			_DbContext = blogDbContext;
 		}
 
 		public async Task<List<Blog>> GetAllAsync()
 		{
-			return await _blogDbContext.Blogs.ToListAsync();
+			return await _DbContext.Blogs.ToListAsync();
 		}
 
 		public async Task<Blog> GetByIdAsync(int id)
 		{
-			return await _blogDbContext.Blogs.AsNoTracking()
+			return await _DbContext.Blogs.AsNoTracking()
 				.FirstOrDefaultAsync(b => b.Id == id);
 		}
 		public async Task<Blog> CreateAsync(Blog blog)
 		{
-			await _blogDbContext.Blogs.AddAsync(blog);
-			await _blogDbContext.SaveChangesAsync();
+			await _DbContext.Blogs.AddAsync(blog);
+			await _DbContext.SaveChangesAsync();
 			return blog;
 		}
 
 		public async Task<int> DeleteAsync(int id)
 		{
-			return await _blogDbContext.Blogs
+			return await _DbContext.Blogs
 				  .Where(model => model.Id == id)
 			.ExecuteDeleteAsync();
 		}
@@ -45,7 +45,7 @@ namespace CleanArchitecture.Infrastructure.Repositories
 		
 		public async Task<int> UpdateAsync(int id, Blog blog)
 		{
-			return await _blogDbContext.Blogs
+			return await _DbContext.Blogs
 				  .Where(model => model.Id == id)
 				  .ExecuteUpdateAsync(setters => setters
 					.SetProperty(m => m.Id, blog.Id)
