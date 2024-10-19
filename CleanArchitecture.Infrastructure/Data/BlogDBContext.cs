00ada299df32bda5e22cd465754c6f68d623416f
Pranavsolanki01 <pranavsolanki200@gmail.com>
2024-10-19 15:39:54 +0530

    Add project files.

---
+++ CleanArchitecture.Infrastructure/Data/BlogDBContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data
+{
+	public class BlogDbContext : DbContext
+	{
+		public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+
+		public DbSet<Blog> Blogs { get; set; }
+	}
+}
